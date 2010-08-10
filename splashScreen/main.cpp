a7f1b66f15b84152c5cae60838e680fcbf784715
Erwan Demairy <edemairy@microscopium.inria.fr>
2010-08-10 14:59:13 +0200

    addition of the source files for the splash screen example.

---
+++ splashScreen/main.cpp
@@ -0,0 +1,28 @@
+#include <QApplication>
+#include <QMainWindow>
+#include <QPixmap>
+#include <QSplashScreen>
+int main(int argc, char *argv[])
+ {
+     QApplication app(argc, argv);
+//     QPixmap pixmap(":/splash.png");
+     QPixmap pixmap("/home/edemairy/devel/hello_world.png");
+     QSplashScreen splash(pixmap);
+     splash.show();
+     app.processEvents();
+     QMainWindow window;
+     window.show();
+    
+     sleep(5);
+     splash.showMessage("Loaded modules");
+     qApp->processEvents();
+
+     sleep(5);
+     // Establishing connections
+     splash.showMessage("Established connections");
+     qApp->processEvents();
+     
+     sleep(5);
+     splash.finish(&window);
+     return app.exec();
+ }
